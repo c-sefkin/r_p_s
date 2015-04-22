1452665004995b306e539ab9417c3590119896e3
Chris Sefkin <c.sefkin@gmail.com>
2015-04-22 16:06:05 -0700

    final working version

--- lib/r_p_s.rb
+++ lib/r_p_s.rb
@@ -1,14 +1,9 @@
-class Test
-  RULES = {
-      :rock => {:rock => :draw, :paper => :paper, :scissors => :rock},
-      :paper => {:rock => :paper, :paper => :draw, :scissors => :scissors},
-      :scissors => {:rock => :rock, :paper => :scissors, :scissors => :draw},
-    }
+RULES = {
+    :rock => {:rock => :draw, :paper => :paper, :scissors => :rock},
+    :paper => {:rock => :paper, :paper => :draw, :scissors => :scissors},
+    :scissors => {:rock => :rock, :paper => :scissors, :scissors => :draw},
+  }
 
-    def play(p1,p2)
-      RULES[p1][p2]
-    end
-    puts play(:rock, :paper)
-    puts play(:scissors, :rock)
-    puts play(:scissors, :scissors)
-end
+  def play(p1,p2)
+    RULES[p1][p2]
+  end
