636fcb5aac15181f2e44eb63df8723f710e0e321
Chris Sefkin <c.sefkin@gmail.com>
2015-04-22 14:03:44 -0700

    updated main rb file

--- lib/r_p_s.rb
+++ lib/r_p_s.rb
@@ -1,4 +1,11 @@
 class String
   define_method(:r_p_s) do
-  end
-  
+    rules = {
+      :rock => {:rock => draw, :paper => paper, :scissors => rock}
+      :paper => {:rock => paper, :paper => draw, :scissors => scissors}
+      :scissors => {:rock => rock, :paper => scissors, :scissors => draw }
+
+
+    }
+end
+end
