942ba163264be78ca46f8720b87b0ea3e0960a2b
Chris Sefkin <c.sefkin@gmail.com>
2015-04-22 15:19:17 -0700

    it's working! for now.

--- lib/r_p_s.rb
+++ lib/r_p_s.rb
@@ -1,11 +1,14 @@
-class String
-  define_method(:r_p_s) do
-    rules = {
-      :rock => {:rock => draw, :paper => paper, :scissors => rock}
-      :paper => {:rock => paper, :paper => draw, :scissors => scissors}
-      :scissors => {:rock => rock, :paper => scissors, :scissors => draw }
-
-
+class Test
+  RULES = {
+      :rock => {:rock => :draw, :paper => :paper, :scissors => :rock},
+      :paper => {:rock => :paper, :paper => :draw, :scissors => :scissors},
+      :scissors => {:rock => :rock, :paper => :scissors, :scissors => :draw},
     }
-end
+
+    def play(p1,p2)
+      RULES[p1][p2]
+    end
+    puts play(:rock, :paper)
+    puts play(:scissors, :rock)
+    puts play(:scissors, :scissors)
 end
