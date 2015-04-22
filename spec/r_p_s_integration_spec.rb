50f04c5d30733b401d66f81f711911e107857315
Chris Sefkin <c.sefkin@gmail.com>
2015-04-22 16:36:08 -0700

    working integration testing

--- spec/r_p_s_integration_spec.rb
+++ spec/r_p_s_integration_spec.rb
@@ -5,7 +5,7 @@ Capybara.app = Sinatra::Application
 describe('the rock paper scissors path', {:type => :feature}) do
   it('processes the multiple user entries and returns the winning action') do
     visit('/')
-    fill_in('title', :with => 'rock,paper')
+    fill_in('title1' && 'title2', :with => 'rock,paper')
     click_button('Send')
     expect(page).to have_content('paper')
   end
